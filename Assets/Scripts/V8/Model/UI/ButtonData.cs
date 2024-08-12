4d8087c9f57835bcaf98db33062f569b92aaad03
sb.kim <seungbae@planetariumhq.com>
2024-08-12 09:26:29 +0900

    feat: Work in progress: Implementing data for UI ButtonData
    - Currently developing the data structure and logic for `ButtonData` within the UI.

--- Assets/Scripts/V8/Model/UI/ButtonData.cs
+++ Assets/Scripts/V8/Model/UI/ButtonData.cs
@@ -7,11 +7,31 @@ namespace V8
     [Serializable]
     public class ButtonData : FrameData
     {
+        public enum ButtonType
+        {
+            IMAGE_BUTTON,
+            TEXT_BUTTON,
+        }
+
+        [JsonProperty("buttonType", Required = Required.Always)]
+        public ButtonType buttonType = ButtonType.IMAGE_BUTTON;
+
         [JsonProperty("events", Required = Required.Always)]
         public Dictionary<string, string> events;
+
         [JsonProperty("threshold", Required = Required.Default)]
         public float threshold = 0.5f;
-        // public string type = "none";    // image, text, none
-        // public string normalSpriteId;
+
+        [JsonProperty("defaultSpriteId", Required = Required.Always)]
+        public string defaultSpriteId;
+
+        [JsonProperty("hoverSpriteId", Required = Required.Default)]
+        public string hoverSpriteId;
+
+        [JsonProperty("pressedSpriteId", Required = Required.Default)]
+        public string pressedSpriteId;
+        
+        // [JsonProperty("text", Required = Required.Default)]
+        // public string text;
     }
 }
