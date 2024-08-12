c3429dc1ffd3b16124e7e65f6f8b75ce98ddaeaf
sb.kim <seungbae@planetariumhq.com>
2024-08-12 18:09:02 +0900

    fix: Modify Image and Button component creation method
    - Updated the component creation logic for Image and Button components.
    - Changed from creating components on child GameObjects to creating them on the current GameObject itself.

--- Assets/Scripts/V8/UI/Component/ImageComponents.cs
+++ Assets/Scripts/V8/UI/Component/ImageComponents.cs
@@ -8,15 +8,14 @@ namespace V8
 
         public ImageComponents(IElement parent, string name) : base(parent, name)
         {
-            var go = new GameObject(UIConfig.Element);
-            var element = go.AddComponent<RectTransform>();
+            var element = Self;
             element.SetParent(Self);
             element.anchorMin = Vector2.zero;
             element.anchorMax = Vector2.one;
             element.offsetMin = Vector2.zero;
             element.offsetMax = Vector2.zero;
 
-            Image = go.gameObject.AddComponent<UnityEngine.UI.Image>();
+            Image = element.gameObject.AddComponent<UnityEngine.UI.Image>();
         }
     }
 }
--- Assets/Scripts/V8/UI/Component/LabelComponents.cs
+++ Assets/Scripts/V8/UI/Component/LabelComponents.cs
@@ -9,15 +9,14 @@ namespace V8
 
         public LabelComponents(IElement parent, string name) : base(parent, name)
         {
-            var go = new GameObject(UIConfig.Element);
-            var element = go.AddComponent<RectTransform>();
+            var element = Self;
             element.SetParent(Self);
             element.anchorMin = Vector2.zero;
             element.anchorMax = Vector2.one;
             element.offsetMin = Vector2.zero;
             element.offsetMax = Vector2.zero;
 
-            TMP = go.AddComponent<TextMeshProUGUI>();
+            TMP = element.gameObject.AddComponent<TextMeshProUGUI>();
         }
     }
 }
