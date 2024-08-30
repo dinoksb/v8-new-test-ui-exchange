f4681ca824a87dd20ea3320069f45c31e222e067
sb.kim <seungbae@planetariumhq.com>
2024-08-30 18:38:47 +0900

    feat: Add background image handling logic

--- Assets/Scripts/V8/UI/Component/ImageComponents.cs
+++ Assets/Scripts/V8/UI/Component/ImageComponents.cs
@@ -4,18 +4,29 @@ namespace V8
 {
     public class ImageComponents : FrameComponents
     {
+        public UnityEngine.UI.Image BackGroundImage { get; }
         public UnityEngine.UI.Image Image { get; }
 
         public ImageComponents(IElement parent, string name) : base(parent, name)
         {
-            var element = Self;
-            element.SetParent(Self);
-            element.anchorMin = Vector2.zero;
-            element.anchorMax = Vector2.one;
-            element.offsetMin = Vector2.zero;
-            element.offsetMax = Vector2.zero;
+            var bgElement = Self;
+            bgElement.SetParent(Self);
+            bgElement.anchorMin = Vector2.zero;
+            bgElement.anchorMax = Vector2.one;
+            bgElement.offsetMin = Vector2.zero;
+            bgElement.offsetMax = Vector2.zero;
 
-            Image = element.gameObject.AddComponent<UnityEngine.UI.Image>();
+            var imageGo = new GameObject("SourceImage");
+            var imageElement = imageGo.AddComponent<RectTransform>();
+            imageElement.SetParent(bgElement);
+            imageElement.anchorMin = Vector2.zero;
+            imageElement.anchorMax = Vector2.one;
+            imageElement.offsetMin = Vector2.zero;
+            imageElement.offsetMax = Vector2.zero;
+            
+
+            BackGroundImage = bgElement.gameObject.AddComponent<UnityEngine.UI.Image>();
+            Image = imageElement.gameObject.AddComponent<UnityEngine.UI.Image>();
         }
     }
 }
