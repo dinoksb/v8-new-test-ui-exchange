732cec57baafd8040b1122d451e764bceaec902a
sb.kim <seungbae@planetariumhq.com>
2024-08-30 18:38:47 +0900

    feat: Add background image handling logic

--- Assets/Scripts/V8/Model/UI/ImageData.cs
+++ Assets/Scripts/V8/Model/UI/ImageData.cs
@@ -6,9 +6,12 @@ namespace V8
     [Serializable]
     public class ImageData : FrameData
     {
-        [JsonProperty("spriteId", Required = Required.Default)]
+        [JsonProperty("spriteId", Required = Required.Always)]
         public string spriteId;
-
+        
+        [JsonProperty("backgroundColor", Required = Required.Default)]
+        public float[] backgroundColor = new float[] { 255, 255, 255, 255 };
+        
         [JsonProperty("imageColor", Required = Required.Default)]
         public float[] imageColor = new float[] { 255, 255, 255, 255 };
     }
--- Assets/Scripts/V8/UI/Elements/Basic/Image.cs
+++ Assets/Scripts/V8/UI/Elements/Basic/Image.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 namespace V8
 {
+    // Todo: source Image 변경 기능 및 Raise Event 기능 추가
+    // Todo: source Image Release 기능 추가
     public class Image : Frame
     {
+        private UnityEngine.UI.Image _backgroundImage;
         private UnityEngine.UI.Image _image;
         
         //Todo: Lazy 초기화가 필요할지 고민필요...
@@ -13,15 +16,38 @@ namespace V8
         private readonly List<SorceChangeEventAction> _sourceChangeEvents = new();
         public delegate void ColorChanageEventAction(IElement element, Color prevColor, Color newColor);
         public delegate void SorceChangeEventAction(IElement element, string prevUrl, string newUrl);
-        
+
+        public Color BackgroudColor
+        {
+            get => _backgroundImage.color;
+            set
+            {
+                var prevColor = _backgroundImage.color;
+                _backgroundImage.color = value;
+                foreach (var eventAction in _backgroundColorChangeEvents)
+                {
+                    eventAction?.Invoke(this, prevColor, value);
+                }
+            }
+        }
+
         public Color ImageColor
         {
             get => _image.color;
-            set => _image.color = value;
+            set
+            {
+                var prevColor = _image.color;
+                _image.color = value;
+                foreach (var eventAction in _sourceColorChangeEvents)
+                {
+                    eventAction?.Invoke(this, prevColor, value);
+                }
+            }
         }
 
         public Image(ImageData data, ImageComponents components, Sprite sprite) : base(data, components)
         {
+            _backgroundImage = components.BackGroundImage;
             _image = components.Image;
             _image.type = UnityEngine.UI.Image.Type.Sliced; 
             _image.sprite = sprite;
@@ -45,6 +71,7 @@ namespace V8
 
         private void SetValues(ImageData data)
         {
+            BackgroudColor = TypeConverter.ToColor(data.backgroundColor);
             ImageColor = TypeConverter.ToColor(data.imageColor);
             _image.raycastTarget = data.interactable;
         }
