febd3357aa3c9a3cf11cbc2393ba881985d7fe7f
sb.kim <seungbae@planetariumhq.com>
2024-09-02 18:06:58 +0900

    feat: Modify button event registration functionality

--- Assets/Scripts/V8/UI/Elements/Interactive/Button.cs
+++ Assets/Scripts/V8/UI/Elements/Interactive/Button.cs
@@ -49,12 +49,6 @@ namespace V8
             }
 
             Events = new ReadOnlyDictionary<EventTriggerType, string>(e);
-            
-            _eventTrigger.triggers.Add(CreateEntry(EventTriggerType.PointerEnter, _pointerEnterEvents));
-            _eventTrigger.triggers.Add(CreateEntry(EventTriggerType.PointerExit, _pointerExitEvents));
-            _eventTrigger.triggers.Add(CreateEntry(EventTriggerType.PointerDown, _pointerDownEvents));
-            _eventTrigger.triggers.Add(CreateEntry(EventTriggerType.PointerUp, _pointerUpEvents));
-
         }
 
         public override IElement Copy(RectTransform self, IElement parent)
@@ -78,7 +72,7 @@ namespace V8
         {
             // todo: 기존에 등록한 이벤트 트리거들 모두 제거됨... 추후 _eventTrigger.triggers.Clear() 로직 제거 필요할수도있음..
             _eventTrigger.triggers.Clear();
-            
+
             foreach (var (eventType, eventId) in events)
             {
                 var entry = _eventTrigger.triggers.FirstOrDefault(x => x.eventID == eventType);
@@ -86,7 +80,7 @@ namespace V8
                 {
                     _eventTrigger.triggers.Remove(entry);
                 }
-            
+
                 _eventTrigger.triggers.Add(CreateEntry(eventType, eventId));
             }
         }
@@ -109,11 +103,11 @@ namespace V8
 
                 _lastEventTimes[type] = Time.time;
                 //Todo: Network 로 이벤트 보낼 때 Name 으로 보내는게 맞을지 UID 로 보내는게 맞을지?
-                _action?.Invoke(NetworkManager.Singleton.LocalClientId, Name, type.ToString(), eventId);
+                // _action?.Invoke(NetworkManager.Singleton.LocalClientId, Name, type.ToString(), eventId);
             });
             return entry;
         }
-        
+
         private EventTrigger.Entry CreateEntry(EventTriggerType type, List<Action<IElement>> eventActions)
         {
             var entry = new EventTrigger.Entry
@@ -141,63 +135,91 @@ namespace V8
 
         public void AddPointerEnterListener(Action<IElement> eventAction)
         {
-            if(!_pointerEnterEvents.Contains(eventAction))
-                _pointerEnterEvents.Add(eventAction);
+            if (_pointerEnterEvents.Contains(eventAction)) return;
+
+            var entry = _eventTrigger.triggers.FirstOrDefault(x => x.eventID == EventTriggerType.PointerEnter);
+            if (entry == null)
+            {
+                _eventTrigger.triggers.Add(CreateEntry(EventTriggerType.PointerEnter, _pointerEnterEvents));
+            }
+
+            _pointerEnterEvents.Add(eventAction);
         }
 
         public void RemovePointerEnterListener(Action<IElement> eventAction)
         {
             _pointerEnterEvents.Remove(eventAction);
         }
-        
+
         public void RemoveAllPointerEnterListener(Action<IElement> eventAction)
         {
             _pointerEnterEvents.Clear();
         }
-        
+
         public void AddPointerExitListener(Action<IElement> eventAction)
         {
-            if(!_pointerExitEvents.Contains(eventAction))
-                _pointerExitEvents.Add(eventAction);
+            if (_pointerExitEvents.Contains(eventAction)) return;
+
+            var entry = _eventTrigger.triggers.FirstOrDefault(x => x.eventID == EventTriggerType.PointerExit);
+            if (entry == null)
+            {
+                _eventTrigger.triggers.Add(CreateEntry(EventTriggerType.PointerExit, _pointerExitEvents));
+            }
+
+            _pointerExitEvents.Add(eventAction);
         }
 
         public void RemovePointerExitListener(Action<IElement> eventAction)
         {
             _pointerExitEvents.Remove(eventAction);
         }
-        
+
         public void RemoveAllPointerExitListener(Action<IElement> eventAction)
         {
             _pointerExitEvents.Clear();
         }
-        
+
         public void AddPointerDownListener(Action<IElement> eventAction)
         {
-            if(!_pointerDownEvents.Contains(eventAction))
-                _pointerDownEvents.Add(eventAction);
+            if (_pointerDownEvents.Contains(eventAction)) return;
+
+            var entry = _eventTrigger.triggers.FirstOrDefault(x => x.eventID == EventTriggerType.PointerDown);
+            if (entry == null)
+            {
+                _eventTrigger.triggers.Add(CreateEntry(EventTriggerType.PointerDown, _pointerDownEvents));
+            }
+
+            _pointerDownEvents.Add(eventAction);
         }
 
         public void RemovePointerDownListener(Action<IElement> eventAction)
         {
             _pointerDownEvents.Remove(eventAction);
         }
-        
+
         public void RemoveAllPointerDownListener(Action<IElement> eventAction)
         {
             _pointerDownEvents.Clear();
         }
-        
+
         public void AddPointerUpListener(Action<IElement> eventAction)
         {
-            if(!_pointerUpEvents.Contains(eventAction))
-                _pointerUpEvents.Add(eventAction);
+            if (_pointerUpEvents.Contains(eventAction)) return;
+
+            var entry = _eventTrigger.triggers.FirstOrDefault(x => x.eventID == EventTriggerType.PointerUp);
+            if (entry == null)
+            {
+                _eventTrigger.triggers.Add(CreateEntry(EventTriggerType.PointerUp, _pointerUpEvents));
+            }
+
+            _pointerUpEvents.Add(eventAction);
         }
 
         public void RemovePointerUpListener(Action<IElement> eventAction)
         {
             _pointerUpEvents.Remove(eventAction);
         }
-        
+
         public void RemoveAllPointerUpListener(Action<IElement> eventAction)
         {
             _pointerUpEvents.Clear();
