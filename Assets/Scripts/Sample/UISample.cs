44afce1277cd1105c7fec2123845b193376907f5
sb.kim <seungbae@planetariumhq.com>
2024-09-14 15:44:05 +0900

    feat: Progress UI sample scene development

--- Assets/Scripts/Sample/UISample.cs
+++ Assets/Scripts/Sample/UISample.cs
@@ -2,9 +2,25 @@ using System;
 using UnityEngine;
 using V8;
 using V8.Service;
+using V8.Utilities;
 
 public class UISample : MonoBehaviour
 {
+    public UnityEngine.Canvas UICanvas
+    {
+        get
+        {
+            if (!_uiCanvas)
+            {
+                var element = new V8.Canvas("UICanvas", null, new Vector2(Screen.width, Screen.height), false);
+                _uiCanvas = element.Self.GetComponent<UnityEngine.Canvas>();
+            }
+
+            return _uiCanvas;
+        }
+    }
+    
+    [SerializeField] private string _url;
     [SerializeField] private UIManager _uiManager;
     [SerializeField] private UIService _uiService;
     
@@ -12,20 +28,25 @@ public class UISample : MonoBehaviour
 
     private void Start()
     {
-        
+        LoadUIAsync(_url);
     }
     
-    private async void LoadUIAsync(string uiUrl)
+    private async void LoadUIAsync(string url)
     {
-        var json = await WebRequestUtility.GetData(uiUrl);
-        if (string.IsNullOrEmpty(json))
+        if (string.IsNullOrEmpty(url))
+        {
+            InternalDebug.LogError("url is null or empty");
             return;
-        LoadUI(json);
-    }
-    
-    private async void LoadUI(string jsonData)
-    {
-        await _uiManager.LoadAsync(jsonData);
-        // _uiManager.Show(UICanvasForTest);
+        }
+        
+        var result = await WebRequestUtility.GetData(url);
+        if (string.IsNullOrEmpty(result))
+        {
+            InternalDebug.LogError("result is null");
+            return;
+        }
+        
+        await _uiManager.LoadAsync(result);
+        _uiManager.Show(UICanvas);
     }
 }
