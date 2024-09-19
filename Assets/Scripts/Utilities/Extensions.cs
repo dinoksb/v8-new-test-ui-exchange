9fd8d4daaabc0bc1063b68bf11746c85d03e07fb
sb.kim <seungbae@planetariumhq.com>
2024-09-19 17:24:33 +0900

    feat: add extensions

--- Assets/Scripts/Utilities/Extensions.cs
+++ Assets/Scripts/Utilities/Extensions.cs
@@ -8,6 +8,21 @@ namespace Utilities
         {
             return new Vector2(vec.x, 1 - vec.y);
         }
+        
+        public static bool ApproximatelyEqual(this Vector2 v1, Vector2 v2)
+        {
+            return Vector2.Distance(v1, v2) < Mathf.Epsilon;
+        }
+
+        public static bool ApproximatelyEqual(this Vector3 v1, Vector3 v2)
+        {
+            return Vector3.Distance(v1, v2) < Mathf.Epsilon;
+        }
+        
+        public static bool ApproximatelyEqual(this Quaternion q1, Quaternion q2)
+        {
+            return Quaternion.Angle(q1, q2) < Mathf.Epsilon;
+        }
     }
 }
 
